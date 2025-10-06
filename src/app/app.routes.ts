bdcf680a198c64cd80607de17de67fb240a816ca
Chul Ahn <scahn.x@gmail.com>
2025-10-06 17:48:57 -0400

    change package

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
+// import { renderMode } from '@angular/ssr';
+
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./pages/list/list.component').then(m => m.ListComponent) },
-  { path: 'edit/:id', loadComponent: () => import('./pages/edit/edit.component').then(m => m.EditComponent) },
+  {
+    path: 'edit/:id',
+    loadComponent: () => import('./pages/edit/edit.component').then(m => m.EditComponent),
+    data: { renderMode: 'client' } // ✅ don’t prerender this route
+  },
   { path: '**', redirectTo: '' }
 ];
